e105d69b5fbbfac77ac0d72124d931225af5b92f
ianlhzh <1187563558@qq.com>
2020-05-13 02:39:04 +0800

    前台页面

--- src/components/courseList.vue
+++ src/components/courseList.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="courseList">
 <h1>课程列表</h1>
+    <el-row>
+      <el-col :span="8" v-for="item in courseList" :key="item.id">
+        <el-card>
+          <img :src="item.bigPhoto" class="image">
+          <div style="padding: 1px;">
+            <!-- <span>{{item.name}}</span> -->
+            <div class="bottom clearfix">
+              <el-button type="text"  class="button" @click="downLoadCourse(item)">下载</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -17,12 +30,19 @@ export default {
     //   console.log(abbreviation)
     // },
     getAllCourse (params) {
-      const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+      const config = { headers: { 'Content-Type': 'application/json' } }
       this.axios.post(process.env.API_ROOT + 'course/getAllCourse', params, config).then((response) => {
         console.log(response)
         if (response.data.code === 'SCOM0000') {
-          this.schoolList = response.data.obj
+          this.courseList = response.data.obj.result.list
         }
+      })
+    },
+    downLoadCourse (item) {
+      console.log(item.schoolSN + '-' + item.id)
+      var schoolCourseId = item.schoolSN + '-' + item.id
+      this.axios.get(process.env.API_ROOT + 'crawler/crawlerCourse?schoolCourseId=' + schoolCourseId).then((response) => {
+        console.log(response)
       })
     }
   },
@@ -43,5 +63,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .bottom {
+    margin-top: 3px;
+    line-height: 12px;
+  }
 
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width:88%;
+    display: inline;
+  }
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+
+  .clearfix:after {
+      clear: both
+  }
 </style>
